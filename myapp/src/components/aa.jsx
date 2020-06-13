7a085735f2a03ac6a304e125030acc0218eaf037
fangwentao <1572176528@qq.com>
2020-06-13 17:58:12 +0800

    Tab新建了下面的导航

--- myapp/src/components/aa.jsx
+++ myapp/src/components/aa.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from "react"
 
-
-class Home extends Component {
+export default class aa extends Component {
     render() {
         return (
             <div>
-                Home
+                
             </div>
         )
     }
 }
-export default Home;
