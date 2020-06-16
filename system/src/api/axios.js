18928d133f69200b2bf75076cc4e9822ecdcfcee
ch <3042901539@qq.com>
2020-06-16 14:53:27 +0800

    登录跳转

--- system/src/api/axios.js
+++ system/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import qs from "qs"
 import { message } from "antd"
-axios.defaults.baseURL = "http://localhost:1915/api"
+axios.defaults.baseURL = "http://10.3.135.25:1915/api"
 
 axios.interceptors.request.use((config) => {
     let data = config.data;
